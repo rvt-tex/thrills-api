fe3c2397bf2f22f9553beda476086848271ee835
github username <rvtingling@yahoo.com>
2021-12-16 01:04:04 -0500

    updated README.md

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,5 +1,9 @@
 class ReservationSerializer < ActiveModel::Serializer
 
   attributes :id, :desired_date, :desired_time, :special_request, :client_id, :tour_id
+  
+
+  belongs_to :tour
+  belongs_to :client
 
 end
